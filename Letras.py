4a5b0eac02a993c69dad0c5b5af4a3aabf664f13
EdgardoJCL <92690733+EdgardoJCL@users.noreply.github.com>
2021-10-30 14:14:24 -0500

    Create Letras.py

---
+++ Letras.py
@@ -0,0 +1,11 @@
+#Este código leera si las letras de la palabra estan en mayuscula o en minuscula, y las cambiara, mayusculas a minuscula y alreves.
+def swap_text(s): #nombre de la función
+    result = ""
+    for letter in s: #recorre la palabra, letra por letra, letter nombre de las iteraciones.
+       if letter == letter.upper(): #hace una evaluación diciendo si la letra en la cual estamos es mayuscula
+          result += letter.lower() #las letras nayusculas a minusculas, las letras indicada
+       else: #se da este caso sino funciona el anterior
+          result += letter.upper()
+     print(result)     
+    
+swap_text("hola TODOS") #llamamos a la función
